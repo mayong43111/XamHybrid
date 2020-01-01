978229cc3f40e54560b6b1501433ea0d6c2b12bc
Yanzhi <muslem@msn.com>
2020-01-01 14:32:29 +0800

    Update, Add AppCenter

--- XHApp/XHApp/XHApp.iOS/AppDelegate.cs
+++ XHApp/XHApp/XHApp.iOS/AppDelegate.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using UIKit;
 
 namespace XHApp.iOS
--- XHApp/XHApp/XHApp/App.xaml.cs
+++ XHApp/XHApp/XHApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XHApp.Services;
@@ -15,6 +18,11 @@ namespace XHApp
 
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
+
+            AppCenter.Start("android=708f4271-7e68-43c4-8d27-fbe07e4e88f1;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios=d15c46b2-6708-40e1-8efe-478a44f9deb9",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnStart()
